0099e9102ff064ca86bfd8ebf9ef111efeacea44
5591 <5591>
2020-03-05 18:17:46 -0800

    initial commit

---
+++ DSCC_CW1_5267_Frontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSCC_CW1_5267_Front.MvcApplication" Language="C#" %>
---
+++ DSCC_CW1_5267_Frontend/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DSCC_CW1_5267_Front.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
